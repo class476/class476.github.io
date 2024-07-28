a76a1a85f0c4b80b5028c45c38944990271a9728
Class 476 <168603061+class476@users.noreply.github.com>
2024-07-28 21:34:14 +0800

    Add files via upload

---
+++ assets/css/music.js
@@ -0,0 +1,62 @@
+var playlist = [
+
+    {
+    title:"Smile",
+    artist:"Deenise",
+    mp3:"https://dev.iw233.cn/music/Deenise Smile.mp3",
+    cover:"https://p2.music.126.net/K6pyRG6mdzb5fXhceD3o2g==/109951168772406354.jpg",
+    },
+
+    {
+    title:"Kawaii",
+    artist:"Deenise",
+    mp3:"https://dev.iw233.cn/music/Deenise - Kawaii.mp3",
+    cover:"https://dev.iw233.cn/music/img/Deenise - Kawaii.jpg",
+    },
+
+    {
+    title:"Stream",
+    artist:"Deenise",
+    mp3:"https://dev.iw233.cn/music/Stream Deenise.mp3",
+    cover:"https://dev.iw233.cn/music/img/Stream Deenise.jpg",
+    },
+
+    {
+    title:"On My Grind",
+    artist:"Stessie",
+    mp3:"https://dev.iw233.cn/music/On My Grind - Stessie,Miss Lina.mp3",
+    cover:"https://p1.music.126.net/ds9HbBVNmdu0BaAuXx9avg==/109951167764713115.jpg?param=300x300",
+    },
+
+    {
+    title:"Crossing",
+    artist:"iMeiden",
+    mp3:"https://dev.iw233.cn/music/iMeiden Aki - Crossing (feat. Aki).mp3",
+    cover:"https://p1.music.126.net/e0Vfb6PbJMGVrY8nPpU4TA==/109951166865175082.jpg?param=300x300",
+    },
+
+    {
+    title:"Together",
+    artist:"PLEEG",
+    mp3:"https://dev.iw233.cn/music/PLEEG - Together (feat. Yuca).mp3",
+    cover:"https://p1.music.126.net/pmfvkoTHRMjREN31tfy-pg==/109951164004410097.jpg?param=300x300",
+    },
+
+    {
+    title:"Twilight",
+    artist:"HoneyComeBear",
+    mp3:"https://dev.iw233.cn/music/Twilight - HoneyComeBear.mp3",
+    cover:"https://p2.music.126.net/L-knFaAgOyKF7PteJFZt_g==/109951166869212393.jpg?param=300x300",
+    },
+
+    {
+    title:"With You",
+    artist:"Zero Venture",
+    mp3:"https://dev.iw233.cn/music/Zero Venture - With You.mp3",
+    cover:"https://p1.music.126.net/Qgaz76l2TEeKvgJ5BBcA8g==/1387583680510717.jpg?param=300x300",
+    },
+
+
+    ];
+  var isRotate = true;
+  var autoplay = false;
---
+++ assets/js/music/music.js
@@ -0,0 +1,90 @@
+            var playlist = [
+
+            {
+            title:"Kawaii",
+            artist:"Deenise",
+            mp3:"https://dev.iw233.cn/music/Deenise - Kawaii.mp3",
+            cover:"https://dev.iw233.cn/music/img/Deenise - Kawaii.jpg",
+            },
+
+            {
+            title:"Stream",
+            artist:"Deenise",
+            mp3:"https://dev.iw233.cn/music/Stream Deenise.mp3",
+            cover:"https://dev.iw233.cn/music/img/Stream Deenise.jpg",
+            },
+
+            {
+            title:"On My Grind",
+            artist:"Stessie",
+            mp3:"https://dev.iw233.cn/music/On My Grind - Stessie,Miss Lina.mp3",
+            cover:"http://p1.music.126.net/ds9HbBVNmdu0BaAuXx9avg==/109951167764713115.jpg?param=300x300",
+            },
+
+            {
+            title:"Closer To You",
+            artist:"Stessie",
+            mp3:"https://dev.iw233.cn/music/Closer To You - Stessie.mp3",
+            cover:"http://p1.music.126.net/ds9HbBVNmdu0BaAuXx9avg==/109951167764713115.jpg?param=300x300",
+            },
+
+            {
+            title:"What If",
+            artist:"Stessie",
+            mp3:"https://dev.iw233.cn/music/What If - Stessie.mp3",
+            cover:"http://p1.music.126.net/ds9HbBVNmdu0BaAuXx9avg==/109951167764713115.jpg?param=300x300",
+            },
+
+            {
+            title:"Whoever You Are",
+            artist:"Stessie",
+            mp3:"https://dev.iw233.cn/music/Whoever You Are (feat. Moon Jelly) - Stessie,Moon Jelly.mp3",
+            cover:"http://p1.music.126.net/ds9HbBVNmdu0BaAuXx9avg==/109951167764713115.jpg?param=300x300",
+            },
+
+            {
+            title:"Crossing",
+            artist:"iMeiden",
+            mp3:"https://dev.iw233.cn/music/iMeiden Aki - Crossing (feat. Aki).mp3",
+            cover:"https://p1.music.126.net/e0Vfb6PbJMGVrY8nPpU4TA==/109951166865175082.jpg?param=300x300",
+            },
+
+            {
+            title:"Together",
+            artist:"PLEEG",
+            mp3:"https://dev.iw233.cn/music/PLEEG - Together (feat. Yuca).mp3",
+            cover:"https://p1.music.126.net/pmfvkoTHRMjREN31tfy-pg==/109951164004410097.jpg?param=300x300",
+            },
+
+            {
+            title:"蹇樸倢棰 ",
+            artist:"HoneyComeBear",
+            mp3:"https://dev.iw233.cn/music/HoneyComeBear - 蹇樸倢棰 .mp3",
+            cover:"http://p2.music.126.net/a6hZKlaIJDkSpxJlQlXzag==/109951166869226558.jpg?param=300x300",
+            },
+
+            {
+            title:"Twilight",
+            artist:"HoneyComeBear",
+            mp3:"https://dev.iw233.cn/music/Twilight - HoneyComeBear.mp3",
+            cover:"https://p2.music.126.net/L-knFaAgOyKF7PteJFZt_g==/109951166869212393.jpg?param=300x300",
+            },
+
+            {
+            title:"With You",
+            artist:"Zero Venture",
+            mp3:"https://dev.iw233.cn/music/Zero Venture - With You.mp3",
+            cover:"https://p1.music.126.net/Qgaz76l2TEeKvgJ5BBcA8g==/1387583680510717.jpg?param=300x300",
+            },
+
+            {
+            title:"鍚涖伄澹般伅",
+            artist:"銇仾銇层倝",
+            mp3:"https://dev.iw233.cn/music/鍚涖伄澹般伅 - 銇仾銇层倝.mp3",
+            cover:"https://p2.music.126.net/0t5n51OF5M9_omnU81u5Iw==/18725782532696566.jpg?param=300x300",
+            },
+
+
+            ];
+          var isRotate = true;
+          var autoplay = false;
